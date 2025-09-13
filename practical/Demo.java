8f560ad1c9252dc70e3d9efb711bbfc32706773a
Alok905 <alokranjanjoshi07567@gmail.com>
2025-09-13 22:23:13 +0530

    static block completed

--- practical/Demo.java
+++ practical/Demo.java
@@ -1,21 +1,23 @@
-class Student {
-    int rollno, marks;
-    String name;
+class Test {
+    int a;
+    static int count;
+    static {
+        count = 5;
+        System.out.println("Static block called");
+    }
 
-    Student(int rollno, String name, int marks) {
-        this.rollno = rollno;
-        this.name = name;
-        this.marks = marks;
+    public Test(int a) {
+        this.a = a;
+        System.out.println("Constructor called");
+    }
+
+    public static void statMethod() {
+        System.out.println("Static method called");
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        Student s1 = new Student(101, "Alice", 95);
-        Student s2 = new Student(102, "Bob", 85);
-        Student s3 = new Student(103, "Charlie", 75);
-        Student[] students = { s1, s2, s3 };
-        for (Student s : students)
-            System.out.println("Student " + s.rollno + ": " + s.name + ", " + s.marks);
+        Class.forName("Test");
     }
 }
