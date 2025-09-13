b5c45caadd16aa990718d9ac61e5c82ce97839e3
Alok905 <alokranjanjoshi07567@gmail.com>
2025-09-13 19:23:07 +0530

    array finishe

--- practical/Demo.java
+++ practical/Demo.java
@@ -1,16 +1,21 @@
-class Calculator {
-    int num = 5; // instance variable
+class Student {
+    int rollno, marks;
+    String name;
 
-    public int add(int a, int b) { // local variable
-        System.out.println(num);
-        return a + b;
+    Student(int rollno, String name, int marks) {
+        this.rollno = rollno;
+        this.name = name;
+        this.marks = marks;
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        int r1 = calc.add(5, 6);
-        System.out.println("Sum is: " + r1);
+        Student s1 = new Student(101, "Alice", 95);
+        Student s2 = new Student(102, "Bob", 85);
+        Student s3 = new Student(103, "Charlie", 75);
+        Student[] students = { s1, s2, s3 };
+        for (Student s : students)
+            System.out.println("Student " + s.rollno + ": " + s.name + ", " + s.marks);
     }
 }
